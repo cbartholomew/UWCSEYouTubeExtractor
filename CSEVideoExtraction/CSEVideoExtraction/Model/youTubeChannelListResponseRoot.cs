d0e984a03a88e19e8cfe37a94c84a5612963b700
Christopher Bartholomew <cbartholomew@gmail.com>
2017-03-23 19:06:36 -0700

    added new classes

---
+++ CSEVideoExtraction/CSEVideoExtraction/Model/youTubeChannelListResponseRoot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEVideoExtraction.Model
+{
+    public class youtubeChannelListResponseRoot
+    {        
+        public class Rootobject
+        {
+            public string kind { get; set; }
+            public string etag { get; set; }
+            public Pageinfo pageInfo { get; set; }
+            public Item[] items { get; set; }
+        }
+        
+        public class Pageinfo
+        {
+            public int totalResults { get; set; }
+            public int resultsPerPage { get; set; }
+        }
+        
+        public class Item
+        {
+            public string kind { get; set; }
+            public string etag { get; set; }
+            public string id { get; set; }
+            public Contentdetails contentDetails { get; set; }
+        }
+        
+        public class Contentdetails
+        {
+            public Relatedplaylists relatedPlaylists { get; set; }
+        }
+        
+        public class Relatedplaylists
+        {
+            public string uploads { get; set; }
+            public string watchHistory { get; set; }
+            public string watchLater { get; set; }
+        }
+
+    }
+}
---
+++ CSEVideoExtraction/CSEVideoExtraction/Utility/myJSONHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace CSEVideoExtraction.Utility
+{
+    public class myJSONHelper
+    {
+        public static string Serialize<T>(T obj)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            MemoryStream ms = new MemoryStream();
+            serializer.WriteObject(ms, obj);
+            string retVal = Encoding.UTF8.GetString(ms.ToArray());
+            return retVal;
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            T obj = Activator.CreateInstance<T>();
+            MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            obj = (T)serializer.ReadObject(ms);
+            ms.Close();
+            return obj;
+        }
+
+        public static T Deserialize<T>(Stream jsonStream)
+        {
+            T obj = Activator.CreateInstance<T>();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            obj = (T)serializer.ReadObject(jsonStream);
+            jsonStream.Close();
+            return obj;
+        }
+    }
+    }
+}
